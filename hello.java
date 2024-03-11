2004b2aaa8533eac5bc2864636d5f64464fb7921
iantanui <rutooian@gmail.com>
2024-03-11 11:40:26 +0300

    operators

--- hello.java
+++ hello.java
@@ -2,8 +2,17 @@
  * hello
  */
 public class hello {
-public static void main(String[] args) {
-    System.out.println("Hello!");
-}
-    
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 2;
+
+        System.out.println("a + b = " + (a + b)); // addition
+        System.out.println("a - b = " + (a - b)); // subtraction
+        System.out.println("a * b = " + (a * b)); // multiplication
+        System.out.println("a / b = " + (a / b)); // division
+        System.out.println("a % b = " + (a % b)); // modulo
+    }
+
 }
