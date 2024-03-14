bcfb072996a9a4b3ed1c2798e1545597231c3134
iantanui <rutooian@gmail.com>
2024-03-14 11:52:55 +0300

    relational operators

--- hello.java
+++ hello.java
@@ -9,6 +9,24 @@ class Main {
 
         // value of a nd b
         System.out.println("a is " + a + " and b is " + b); 
+
+        // == operator
+        System.out.println("a == b : " + (a == b)); // false
+
+        // != operator
+        System.out.println("a != b : " + (a != b)); // true
+
+        // > operator
+        System.out.println("a > b : " + (a > b)); // false
+
+        // < operator
+        System.out.println("a < b : " + (a < b)); // true
+
+        // >= operator
+        System.out.println("a >= b : " + (a >= b)); // false
+
+        // <= operator
+        System.out.println("a <= b : " + (a <= b)); // true
     }
 
 }
