a1e135d5190d188942cdc17ea11bca23fbc9128c
iantanui <rutooian@gmail.com>
2024-03-18 14:59:35 +0300

    switch statement

--- hello.java
+++ hello.java
@@ -1,25 +1,39 @@
 
-
 class Main {
 
     public static void main(String[] args) {
 
-        int number = -1;
+        // Java Program to check the size
+        // using the switch...case statement
 
-        // check if number is greater than 0
-        if (number > 0) {
-            System.out.println("The number is positive");
-        }
+        int number = 42;
+        String size;
 
-        // if number is less than 0
-        else if (number < 0) {
-            System.out.print("The number is  negative");
-        }
+        // switch statement to check size
+        switch (number) {
 
-        // if both conditio is false
-        else {
-            System.out.println("The number is 0");
+            case 29:
+                size = "Small";
+                break;
+
+            case 42:
+                size = "Medium";
+                break;
+
+            // match the value of week
+            case 44:
+                size = "Large";
+                break;
+
+            case 48:
+                size = "Extra Large";
+                break;
+
+            default:
+                size = "Unknown";
+                break;
+
         }
+        System.out.println("Size: " + size);
     }
-
 }
